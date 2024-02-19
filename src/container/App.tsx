bfbe2672a927f081fafdb43f3aa204f8417d734c
viniciusalvesc <vinicius.alves@finlytech.com>
2024-02-18 21:10:52 -0300

    feat(master): configuração de linter e debugger

---
+++ src/container/App.tsx
@@ -0,0 +1,5 @@
+export function App() {
+    const teste = 'falar'
+
+    return <h1>header para testes</h1>
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './container/App'
+// import { Provider } from 'react-redux'
+// import store from './store'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+)
